49f9d4bff14b07e7d2763b6fca9ce0840a56b2ff
haalukaa <haleluya.2mahder@gmail.com>
2026-06-26 01:49:09 +0800

    Add instrument data for neuro surgical tools

---
+++ database.js
@@ -0,0 +1,10 @@
+const instruments = [
+  { id: "NEU001", name: "Adson Hook", shelf: "7", location: "56", specialty: "Neuro" },
+  { id: "NEU002", name: "Periosteal Elevator Sharp", shelf: "7", location: "52", specialty: "Neuro" },
+  { id: "NEU003", name: "Posterior Fossa Retractor", shelf: "7 Rear", location: "76", specialty: "Neuro" },
+  { id: "NEU004", name: "Bayonet Forceps Toothed", shelf: "7", location: "58", specialty: "Neuro" },
+  { id: "NEU005", name: "Bayonet Forceps Non-Toothed", shelf: "7", location: "59", specialty: "Neuro" },
+  { id: "NEU006", name: "Brain Retractors Malleables", shelf: "7", location: "57", specialty: "Neuro" },
+  { id: "NEU007", name: "Williams Discectomy Retractor Set", shelf: "7", location: "78", specialty: "Neuro" },
+  { id: "NEU008", name: "Yasargil Tumor Forceps", shelf: "7", location: "64", specialty: "Neuro" }
+];
